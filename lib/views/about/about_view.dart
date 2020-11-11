62703c8b12ff33a852e9134c50ac272086033132
DelhiBoi <52867282+DelhiBoi@users.noreply.github.com>
2020-11-12 00:37:14 +0530

    12 nov

--- lib/views/about/about_view.dart
+++ lib/views/about/about_view.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutView extends StatelessWidget {
   const AboutView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          child: ElevatedButton(
+            onPressed: _launchURL,
+            child: Text('Open My Github'),
+          ),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          child: ElevatedButton(
+            onPressed: _launchURL,
+            child: Text('Open My Github'),
+          ),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          child: ElevatedButton(
+            onPressed: _launchURL,
+            child: Text('Open My Github'),
+          ),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          child: ElevatedButton(
+            onPressed: _launchURL,
+            child: Text('Open My Github'),
+          ),
+        )
+      ],
     );
+  }
+}
+
+_launchURL() async {
+  const url = 'https://github.com/AJ-bitsH';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
